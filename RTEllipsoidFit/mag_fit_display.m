94ff6d681887df147539d5c1559cfeedf91973db
richards-tech <info@richards-tech.com>
2014-10-13 17:57:19 -0400

    Fixed a missing license header in RTEllipsoidFit

--- RTEllipsoidFit/mag_fit_display.m
+++ RTEllipsoidFit/mag_fit_display.m
@@ -1,4 +1,21 @@
-% display
+%//
+%//  Copyright (c) 2014, richards-tech
+%//	
+%//  This file is part of RTEllipsoidFit
+%//
+%//  RTEllipsoidFit is free software: you can redistribute it and/or modify
+%//  it under the terms of the GNU General Public License as published by
+%//  the Free Software Foundation, either version 3 of the License, or
+%//  (at your option) any later version.
+%//
+%//  RTEllipsoidFit is distributed in the hope that it will be useful,
+%//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%//  GNU General Public License for more details.
+%//
+%//  You should have received a copy of the GNU General Public License
+%//  along with RTEllipsoidFit.  If not, see <http://www.gnu.org/licenses/>.
+%//
 
 function mag_fit_display(x, y, z, center, radii, v, xCorr, yCorr, zCorr)
 
