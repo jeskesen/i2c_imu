53d32004ad682daa976b8cdecd9fa94fdadf2a6b
Justin Eskesen <justin.eskesen@gmail.com>
2014-11-11 22:57:01 -0800

    Added device-specific params.

--- src/i2c_imu_node.cpp
+++ src/i2c_imu_node.cpp
@@ -55,15 +55,66 @@ private:
 I2cImu::I2cImu() : nh_(), private_nh_("~")
 {
 
-    private_nh_.param<std::string>("imu_frame_id", imu_frame_id_, "imu_link");
+	// params used for all
+    private_nh_.param<std::string>("frame_id", imu_frame_id_, "imu_link");
 
-    private_nh_.param<int>("imu_type", imu_settings_.m_imuType, RTIMU_TYPE_AUTODISCOVER);
+    private_nh_.param<int>("imu_type", imu_settings_.m_imuType, imu_settings_.m_imuType);
 
-    private_nh_.param<int>("fusion_type", imu_settings_.m_fusionType, RTFUSION_TYPE_RTQF);
+    private_nh_.param<int>("fusion_type", imu_settings_.m_fusionType, imu_settings_.m_fusionType);
     int temp_int;
-    private_nh_.param<int>("i2c_bus", temp_int, 1);
+    private_nh_.param<int>("i2c_bus", temp_int, (int)imu_settings_.m_I2CBus);
     imu_settings_.m_I2CBus = (unsigned char)temp_int;
-    //private_nh_.param<int>("i2c_slave_address", imu_settings_.m_I2CSlaveAddress, 0);
+
+    private_nh_.param<int>("i2c_slave_address", temp_int, (int)imu_settings_.m_I2CSlaveAddress);
+    imu_settings_.m_I2CSlaveAddress = (unsigned char)temp_int;
+
+    //MPU9150
+    private_nh_.param<int>("mpu9150/gyro_accel_sample_rate", imu_settings_.m_MPU9150GyroAccelSampleRate, imu_settings_.m_MPU9150GyroAccelSampleRate);
+    private_nh_.param<int>("mpu9150/compass_sample_rate", imu_settings_.m_MPU9150CompassSampleRate, imu_settings_.m_MPU9150CompassSampleRate);
+    private_nh_.param<int>("mpu9150/accel_full_scale_range", imu_settings_.m_MPU9150AccelFsr, imu_settings_.m_MPU9150AccelFsr);
+    private_nh_.param<int>("mpu9150/gyro_accel_low_pass_filter", imu_settings_.m_MPU9150GyroAccelLpf, imu_settings_.m_MPU9150GyroAccelLpf);
+    private_nh_.param<int>("mpu9150/gyro_full_scale_range", imu_settings_.m_MPU9150GyroFsr, imu_settings_.m_MPU9150GyroFsr);
+
+    //MPU9250
+    private_nh_.param<int>("mpu9250/gyro_accel_sample_rate", imu_settings_.m_MPU9250GyroAccelSampleRate, imu_settings_.m_MPU9250GyroAccelSampleRate);
+    private_nh_.param<int>("mpu9250/compass_sample_rate", imu_settings_.m_MPU9250CompassSampleRate, imu_settings_.m_MPU9250CompassSampleRate);
+    private_nh_.param<int>("mpu9250/accel_full_scale_range", imu_settings_.m_MPU9250AccelFsr, imu_settings_.m_MPU9250AccelFsr);
+    private_nh_.param<int>("mpu9250/accel_low_pass_filter", imu_settings_.m_MPU9250AccelLpf, imu_settings_.m_MPU9250AccelLpf);
+    private_nh_.param<int>("mpu9250/gyro_full_scale_range", imu_settings_.m_MPU9250GyroFsr, imu_settings_.m_MPU9250GyroFsr);
+    private_nh_.param<int>("mpu9250/gyro_low_pass_filter", imu_settings_.m_MPU9250GyroLpf, imu_settings_.m_MPU9250GyroLpf);
+
+    //GD20HM303D
+    private_nh_.param<int>("GD20HM303D/gyro_sample_rate", imu_settings_.m_GD20HM303DGyroSampleRate, imu_settings_.m_GD20HM303DGyroSampleRate);
+    private_nh_.param<int>("GD20HM303D/accel_sample_rate", imu_settings_.m_GD20HM303DAccelSampleRate, imu_settings_.m_GD20HM303DAccelSampleRate);
+    private_nh_.param<int>("GD20HM303D/compass_sample_rate", imu_settings_.m_GD20HM303DCompassSampleRate, imu_settings_.m_GD20HM303DCompassSampleRate);
+    private_nh_.param<int>("GD20HM303D/accel_full_scale_range", imu_settings_.m_GD20HM303DAccelFsr, imu_settings_.m_GD20HM303DAccelFsr);
+    private_nh_.param<int>("GD20HM303D/gyro_full_scale_range", imu_settings_.m_GD20HM303DGyroFsr, imu_settings_.m_GD20HM303DGyroFsr);
+    private_nh_.param<int>("GD20HM303D/compass_full_scale_range", imu_settings_.m_GD20HM303DCompassFsr, imu_settings_.m_GD20HM303DCompassFsr);
+    private_nh_.param<int>("GD20HM303D/accel_low_pass_filter", imu_settings_.m_GD20HM303DAccelLpf, imu_settings_.m_GD20HM303DAccelLpf);
+    private_nh_.param<int>("GD20HM303D/gyro_high_pass_filter", imu_settings_.m_GD20HM303DGyroHpf, imu_settings_.m_GD20HM303DGyroHpf);
+    private_nh_.param<int>("GD20HM303D/gyro_bandwidth", imu_settings_.m_GD20HM303DGyroBW, imu_settings_.m_GD20HM303DGyroBW);
+
+    //GD20M303DLHC
+    private_nh_.param<int>("GD20M303DLHC/gyro_sample_rate", imu_settings_.m_GD20M303DLHCGyroSampleRate, imu_settings_.m_GD20M303DLHCGyroSampleRate);
+    private_nh_.param<int>("GD20M303DLHC/accel_sample_rate", imu_settings_.m_GD20M303DLHCAccelSampleRate, imu_settings_.m_GD20M303DLHCAccelSampleRate);
+    private_nh_.param<int>("GD20M303DLHC/compass_sample_rate", imu_settings_.m_GD20M303DLHCCompassSampleRate, imu_settings_.m_GD20M303DLHCCompassSampleRate);
+    private_nh_.param<int>("GD20M303DLHC/accel_full_scale_range", imu_settings_.m_GD20M303DLHCAccelFsr, imu_settings_.m_GD20M303DLHCAccelFsr);
+    private_nh_.param<int>("GD20M303DLHC/gyro_full_scale_range", imu_settings_.m_GD20M303DLHCGyroFsr, imu_settings_.m_GD20M303DLHCGyroFsr);
+    private_nh_.param<int>("GD20M303DLHC/compass_full_scale_range", imu_settings_.m_GD20M303DLHCCompassFsr, imu_settings_.m_GD20M303DLHCCompassFsr);
+    private_nh_.param<int>("GD20M303DLHC/gyro_high_pass_filter", imu_settings_.m_GD20M303DLHCGyroHpf, imu_settings_.m_GD20M303DLHCGyroHpf);
+    private_nh_.param<int>("GD20M303DLHC/gyro_bandwidth", imu_settings_.m_GD20M303DLHCGyroBW, imu_settings_.m_GD20M303DLHCGyroBW);
+
+
+    //LSM9DS0
+    private_nh_.param<int>("LSM9DS0/gyro_sample_rate", imu_settings_.m_LSM9DS0GyroSampleRate, imu_settings_.m_LSM9DS0GyroSampleRate);
+    private_nh_.param<int>("LSM9DS0/accel_sample_rate", imu_settings_.m_LSM9DS0AccelSampleRate, imu_settings_.m_LSM9DS0AccelSampleRate);
+    private_nh_.param<int>("LSM9DS0/compass_sample_rate", imu_settings_.m_LSM9DS0CompassSampleRate, imu_settings_.m_LSM9DS0CompassSampleRate);
+    private_nh_.param<int>("LSM9DS0/accel_full_scale_range", imu_settings_.m_LSM9DS0AccelFsr, imu_settings_.m_LSM9DS0AccelFsr);
+    private_nh_.param<int>("LSM9DS0/gyro_full_scale_range", imu_settings_.m_LSM9DS0GyroFsr, imu_settings_.m_LSM9DS0GyroFsr);
+    private_nh_.param<int>("LSM9DS0/compass_full_scale_range", imu_settings_.m_LSM9DS0CompassFsr, imu_settings_.m_LSM9DS0CompassFsr);
+    private_nh_.param<int>("LSM9DS0/accel_low_pass_filter", imu_settings_.m_LSM9DS0AccelLpf, imu_settings_.m_LSM9DS0AccelLpf);
+    private_nh_.param<int>("LSM9DS0/gyro_high_pass_filter", imu_settings_.m_LSM9DS0GyroHpf, imu_settings_.m_LSM9DS0GyroHpf);
+    private_nh_.param<int>("LSM9DS0/gyro_bandwidth", imu_settings_.m_LSM9DS0GyroBW, imu_settings_.m_LSM9DS0GyroBW);
 
     imu_ = RTIMU::createIMU(&imu_settings_);
     if(imu_==NULL)
